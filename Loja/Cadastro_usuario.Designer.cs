b79c185f01b423f648dd0367f4d36fb5b7cc4d9c
NirsoItu <48810680+NirsoItu@users.noreply.github.com>
2020-01-02 11:49:01 -0200

    Primeiro commit

---
+++ Loja/Cadastro_usuario.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace Loja
+{
+    partial class Cadastro_usuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.txtCadastro = new System.Windows.Forms.TextBox();
+            this.cboSituacao = new System.Windows.Forms.ComboBox();
+            this.cboPerfil = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnDeletar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(205, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cadastro de Usuários";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Login:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(21, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "E-mail:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(20, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Senha:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(20, 177);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Data Cadastro:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(378, 178);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Situação:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(20, 209);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 16);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Perfil:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(132, 52);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(455, 22);
+            this.txtNome.TabIndex = 8;
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLogin.Location = new System.Drawing.Point(132, 82);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(455, 22);
+            this.txtLogin.TabIndex = 9;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(132, 112);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(455, 22);
+            this.txtEmail.TabIndex = 10;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.Location = new System.Drawing.Point(132, 142);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(133, 22);
+            this.txtSenha.TabIndex = 11;
+            this.txtSenha.UseSystemPasswordChar = true;
+            // 
+            // txtCadastro
+            // 
+            this.txtCadastro.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCadastro.Location = new System.Drawing.Point(132, 172);
+            this.txtCadastro.Name = "txtCadastro";
+            this.txtCadastro.Size = new System.Drawing.Size(133, 22);
+            this.txtCadastro.TabIndex = 12;
+            // 
+            // cboSituacao
+            // 
+            this.cboSituacao.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboSituacao.FormattingEnabled = true;
+            this.cboSituacao.Items.AddRange(new object[] {
+            "Ativo",
+            "Inativo"});
+            this.cboSituacao.Location = new System.Drawing.Point(452, 172);
+            this.cboSituacao.Name = "cboSituacao";
+            this.cboSituacao.Size = new System.Drawing.Size(133, 24);
+            this.cboSituacao.TabIndex = 13;
+            // 
+            // cboPerfil
+            // 
+            this.cboPerfil.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboPerfil.FormattingEnabled = true;
+            this.cboPerfil.Items.AddRange(new object[] {
+            "Administrador",
+            "Operador",
+            "Gerencial"});
+            this.cboPerfil.Location = new System.Drawing.Point(132, 204);
+            this.cboPerfil.Name = "cboPerfil";
+            this.cboPerfil.Size = new System.Drawing.Size(133, 24);
+            this.cboPerfil.TabIndex = 14;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 235);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(564, 188);
+            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnNovo.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNovo.Location = new System.Drawing.Point(24, 453);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(99, 38);
+            this.btnNovo.TabIndex = 16;
+            this.btnNovo.Text = "NOVO";
+            this.btnNovo.UseVisualStyleBackColor = false;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.Color.Yellow;
+            this.btnEditar.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.Location = new System.Drawing.Point(140, 453);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(99, 38);
+            this.btnEditar.TabIndex = 17;
+            this.btnEditar.Text = "EDITAR";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnDeletar
+            // 
+            this.btnDeletar.BackColor = System.Drawing.Color.Red;
+            this.btnDeletar.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeletar.Location = new System.Drawing.Point(256, 453);
+            this.btnDeletar.Name = "btnDeletar";
+            this.btnDeletar.Size = new System.Drawing.Size(99, 38);
+            this.btnDeletar.TabIndex = 18;
+            this.btnDeletar.Text = "DELETAR";
+            this.btnDeletar.UseVisualStyleBackColor = false;
+            this.btnDeletar.Click += new System.EventHandler(this.btnDeletar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.Yellow;
+            this.btnCancelar.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(372, 453);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(99, 38);
+            this.btnCancelar.TabIndex = 19;
+            this.btnCancelar.Text = "CANCELAR";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.BackColor = System.Drawing.Color.Chartreuse;
+            this.btnConfirmar.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirmar.Location = new System.Drawing.Point(488, 453);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(99, 38);
+            this.btnConfirmar.TabIndex = 20;
+            this.btnConfirmar.Text = "CONFIRMAR";
+            this.btnConfirmar.UseVisualStyleBackColor = false;
+            this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.ForeColor = System.Drawing.Color.Red;
+            this.lblMensagem.Location = new System.Drawing.Point(295, 36);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(0, 13);
+            this.lblMensagem.TabIndex = 21;
+            // 
+            // Cadastro_usuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 547);
+            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnDeletar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cboPerfil);
+            this.Controls.Add(this.cboSituacao);
+            this.Controls.Add(this.txtCadastro);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Cadastro_usuario";
+            this.Text = "Cadastro_usuario";
+            this.Load += new System.EventHandler(this.Cadastro_usuario_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.TextBox txtCadastro;
+        private System.Windows.Forms.ComboBox cboSituacao;
+        private System.Windows.Forms.ComboBox cboPerfil;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnDeletar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.Label lblMensagem;
+    }
+}
---
+++ Loja/Cadastro_usuario.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Loja.DTO;
+using Loja.BLL;
+
+namespace Loja
+{
+    public partial class Cadastro_usuario : Form
+    {
+        string modo = "";
+
+        int codUsuSelecionado = -1;
+
+        public Cadastro_usuario()
+        {
+            InitializeComponent();
+        }
+
+        private void Cadastro_usuario_Load(object sender, EventArgs e)
+        {
+            carregaGrid();
+            lblMensagem.Text = "";
+        }
+
+        private void carregaGrid()
+        {
+            try
+            {
+                IList<usuario_DTO> listUsuario_DTO = new List<usuario_DTO>();
+                listUsuario_DTO = new UsuarioBLL().cargaUsuario();
+                /*Preencher dados no DataGridView*/
+                dataGridView1.DataSource = listUsuario_DTO;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            /*Linha atual que estiver selecionada aparecerá nos campos (textbox) * acima do dataGrid*/
+            int sel = dataGridView1.CurrentRow.Index; /*Valor de cada datagrid será enviado ao seu respectivo texbox*/
+            txtNome.Text = Convert.ToString(dataGridView1["nome", sel].Value);
+            txtLogin.Text = Convert.ToString(dataGridView1["login", sel].Value);
+            txtEmail.Text = Convert.ToString(dataGridView1["email", sel].Value);
+            txtSenha.Text = Convert.ToString(dataGridView1["senha", sel].Value);
+            txtCadastro.Text = Convert.ToString(dataGridView1["Cadastro", sel].Value);
+            codUsuSelecionado = Convert.ToInt32(dataGridView1["cod_usuario", sel].Value);
+
+            /*Condição se a situação for igual a "A" então o combobox ficará * Ativo senao "Inativo" */
+            if (Convert.ToString(dataGridView1["situacao", sel].Value) == "A")
+            {
+                cboSituacao.Text = "Ativo";
+            }
+            else
+            {
+                cboSituacao.Text = "Inativo";
+            }
+
+            switch (Convert.ToString(dataGridView1["perfil", sel].Value))
+            {
+                case "1":
+                    cboPerfil.Text = "Administrador";
+                    break;
+
+                case "2":
+                    cboPerfil.Text = "Operador";
+                    break;
+
+                case "3":
+                    cboPerfil.Text = "Gerencial";
+                    break;
+            }
+
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            limpar_campos();
+            txtCadastro.Text = Convert.ToString(System.DateTime.Now);
+            modo = "novo";
+        }
+
+        private void limpar_campos()
+        {
+            txtNome.Text = "";
+            txtLogin.Text = "";
+            txtEmail.Text = "";
+            txtSenha.Text = "";
+            txtCadastro.Text = "";
+            cboPerfil.Text = "";
+            cboSituacao.Text = "";
+        }
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        { 
+        if (modo == "novo") 
+            { 
+            /*Tratamento de Erros, exibe msg*/ 
+            try 
+            { 
+            /*Objeto USU*/ 
+            usuario_DTO USU = new usuario_DTO();
+            USU.nome = txtNome.Text; 
+            USU.login = txtLogin.Text; 
+            USU.email = txtEmail.Text; 
+            USU.cadastro = System.DateTime.Now; 
+            USU.senha = txtSenha.Text;
+
+            if (cboSituacao.Text == "Ativo") 
+            { 
+            USU.situacao = "A";
+            } 
+            else 
+            { 
+            USU.situacao = "I"; 
+            }
+            switch (cboPerfil.Text) 
+            {
+            case "Administrador": USU.perfil = 1; 
+            break; 
+            case "Operador": USU.perfil = 2; 
+            break; 
+            case "Gerencial": USU.perfil = 3; 
+            break; 
+            } 
+            /*Método insere usuário na classe UsuarioBLL*/ 
+            int x = new UsuarioBLL().insereUsuario(USU); 
+            /*Verifica se houve alguma gravação*/ 
+            if (x > 0) 
+                { 
+                MessageBox.Show("Usuário gravado com sucesso!"); 
+                } 
+                /*Recarrega o Grid após os dados serem gravados*/ 
+                carregaGrid(); 
+            } 
+            catch (Exception ex) 
+                { 
+                MessageBox.Show("Erro inesperado " + ex.Message); 
+                } 
+                } 
+            if (modo == "altera") 
+            { 
+            /*Tratamento de Erros, exibe msg*/ 
+            try 
+            { 
+            if (codUsuSelecionado< 0) 
+            { 
+            lblMensagem.Text = "Selecione um usuário antes de prosseguir"; 
+            return; 
+            } 
+            /*Objeto USU, assim como feito no modo="novo" Lê os textbox com os dados alterados*/ 
+            usuario_DTO USU = new usuario_DTO();
+            USU.cod_usuario = codUsuSelecionado; 
+            USU.nome = txtNome.Text; 
+            USU.login = txtLogin.Text;
+            USU.email = txtEmail.Text;
+            USU.cadastro = System.DateTime.Now;
+            USU.senha = txtSenha.Text; 
+
+            if (cboSituacao.Text == "Ativo") 
+            { USU.situacao = "A"; 
+            } 
+            else 
+            { 
+            USU.situacao = "I"; 
+            } 
+            switch (cboPerfil.Text) 
+            { 
+            case "Administrador": USU.perfil = 1; 
+            break; 
+            case "Operador": USU.perfil = 2; 
+            break; 
+            case "Gerencial": USU.perfil = 3; 
+            break; 
+            } 
+            int x = new UsuarioBLL().editaUsuario(USU); 
+            /*Verifica se houve alguma gravação*/
+            if (x > 0) 
+            { 
+            MessageBox.Show("Usuário alterado com Sucesso!"); 
+            } 
+            /*Recarrega o Grid após os dados serem gravados*/ 
+            carregaGrid(); 
+            } 
+            catch (Exception ex) 
+            { 
+            MessageBox.Show("Erro inesperado " + ex.Message); 
+            } 
+            } 
+            if (modo == "excluir") 
+            { 
+            try 
+            { 
+            if (codUsuSelecionado< 0) 
+                         { 
+            lblMensagem.Text = "Selecione um usuário antes de prosseguir"; 
+            return; 
+            } 
+            usuario_DTO USU = new usuario_DTO();
+            USU.cod_usuario = codUsuSelecionado; 
+            int x = new UsuarioBLL().deletaUsuario(USU); 
+            if (x > 0) { MessageBox.Show("Usuário excluido com sucesso!"); 
+            } 
+            /*Recarrega o Grid após os dados serem gravados*/ 
+            carregaGrid();
+            limpar_campos();
+
+            } 
+            catch (Exception ex) 
+            { 
+            MessageBox.Show("Erro inesperado" + ex.Message); 
+            } 
+            } 
+            modo = ""; 
+        } 
+
+            private void btnEditar_Click(object sender, EventArgs e)
+                        {
+                            if (codUsuSelecionado < 0)
+                            {
+                                lblMensagem.Text = "Selecione um usuário antes de prosseguir";
+                                return;
+                            }
+                            modo = "altera";
+                        }
+
+            private void btnDeletar_Click(object sender, EventArgs e)
+            {
+                if (codUsuSelecionado < 0)
+                {
+                    lblMensagem.Text = "Selecione um usuário antes de prosseguir";
+                    return;
+                }
+                else
+                { lblMensagem.Text = "";
+                    modo = "excluir";
+                    MessageBox.Show("Para excluir o usuário, clique em Confirmar");
+                }
+            }
+
+            private void btnCancelar_Click(object sender, EventArgs e)
+            {
+                modo = "";
+                limpar_campos();
+            }
+        }
+    }
+
